a0d6bd96d8cdbd7083c55ea66a8a6d7b568b7a9a
Cole Gibbs <colegibbs0@gmail.com>
2022-03-21 16:51:19 -0700

    UserForm.js configureation for form

---
+++ src/UserForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+class UserForm extends React.Component {
+
+  render() {
+    return(
+      <Form onSubmit={this.props.getCityData}>
+      <Form.Group controlId="city">
+        <Form.Label>City Name</Form.Label>
+        <Form.Control onInput={this.props.handleCity} type="text"/>
+      </Form.Group>
+        <Button 
+          variant="primary" 
+          type="submit">
+          Explore!
+        </Button>
+    </Form>
+    );
+  }
+}
+
+export default UserForm;

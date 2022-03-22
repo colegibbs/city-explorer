952095e7486d31257635acebd42812f4cec4d77d
Cole Gibbs <colegibbs0@gmail.com>
2022-03-21 17:44:45 -0700

    import UserForm.css

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import './UserForm.css';
 
 class UserForm extends React.Component {
 

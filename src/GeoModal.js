5a4e2d2ea4585b4d95a5d8a43568c6be77a445bf
Cole Gibbs <colegibbs0@gmail.com>
2022-03-22 21:18:41 -0700

    added map to loop over weather data and create multiple Weather compoenets with correct data passed

--- src/GeoModal.js
+++ src/GeoModal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Weather from './Weather';
 import Modal from 'react-bootstrap/Modal';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Image from 'react-bootstrap/Image';
@@ -6,6 +7,17 @@ import './GeoModal.css';
 
 class GeoModal extends React.Component {
   render() {
+
+    let weathers = this.props.weatherData.map((day, idx) => {
+      return(
+        <Weather
+          date={day.date}
+          weather={day.description}
+          key={idx}
+        />
+      );
+    })
+
     return(
       <Modal show={this.props.showModal} onHide={this.props.handleClose}>
         <Modal.Header closeButton>
@@ -13,10 +25,11 @@ class GeoModal extends React.Component {
         </Modal.Header>
           <Modal.Body>
             <ListGroup>
-              <ListGroup>Latitude: {this.props.cityData.lat}</ListGroup>
-              <ListGroup>Longitude: {this.props.cityData.lon}</ListGroup>
+              <ListGroup.Item>Latitude: {this.props.cityData.lat}</ListGroup.Item>
+              <ListGroup.Item>Longitude: {this.props.cityData.lon}</ListGroup.Item>
             </ListGroup>
             <Image src={this.props.mapURL}/>
+            {weathers}
           </Modal.Body>
       </Modal>
     );
